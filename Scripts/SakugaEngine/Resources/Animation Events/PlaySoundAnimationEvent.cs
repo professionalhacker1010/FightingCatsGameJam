7db22924530e63d7f8d28c1625ac88e1edd310de
dolphog <mattleung37@gmail.com>
2026-05-05 09:07:01 -0700

    ken lau standing kicks setup

--- Scripts/SakugaEngine/Resources/Animation Events/PlaySoundAnimationEvent.cs
+++ Scripts/SakugaEngine/Resources/Animation Events/PlaySoundAnimationEvent.cs
@@ -3,14 +3,14 @@ using System;
 
 namespace SakugaEngine.Resources
 {
-    [GlobalClass]
-    public partial class PlaySoundAnimationEvent : AnimationEvent
-    {
-        [Export] public Global.SoundType SoundType;
-        [Export] public int Source;
-        [Export] public int Index;
-        [Export] public bool IsRandom;
-        [Export] public int Range;
-        [Export] public int FromExtraVariable = -1;
-    }
+	[GlobalClass]
+	public partial class PlaySoundAnimationEvent : AnimationEvent
+	{
+		[Export] public Global.SoundType SoundType;
+		[Export] public int Source;
+		[Export] public int Index;
+		[Export] public bool IsRandom;
+		[Export] public int Range;
+		[Export] public int FromExtraVariable = -1;
+	}
 }
